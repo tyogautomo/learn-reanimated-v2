e7b68f02535f5a6a25e13ef3d768c38560ce9d60
tyogautomo <yoga.utomo27@gmail.com>
2021-07-21 00:38:24 +0700

    navigation stack

--- src/navigations/mainNavigator.tsx
+++ src/navigations/mainNavigator.tsx
@@ -1,1 +1,14 @@
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const MainNavigator: React.FC<{ name: string }> = ({ name }) => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="PanGesture" component={''} />
+    </Stack.Navigator>
+  );
+};
+
+export { MainNavigator };
